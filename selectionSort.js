77b567bdee0fde552684d24979405c7120276c1f
HG2407 <132276798+HG2407@users.noreply.github.com>
2023-07-08 14:58:07 +0530

    modified selection sort commited
    selection sort modified to sort names

--- selectionSort.js
+++ selectionSort.js
@@ -1,5 +1,6 @@
 window.addEventListener('load', onLoad);
-let array = [10, 2, 29, 20, 17, 65, 42];
+// let array = ['1', 2, 2, 45, 45, 1, 0];
+let array = ['Hitesh', 'Jayshree', 'Yajush', 'Neema', 'Harshit', 'Harsh'];
 function onLoad(){
     let sortedArray = sorter(array);
     console.log(sortedArray);
@@ -16,13 +17,39 @@ function sorter(arr){
     let minIndex;
     for(let i=0; i<length-1; i++){
         minIndex = i;
-        for(let j=i; j<length; j++){
-            if(arr[minIndex]>arr[j]){
-                minIndex = j;
+        for(let j=i+1; j<length; j++){
+            if(compareString(arr[minIndex], arr[j])){//returns true if the firstString is greater than the secondString
+                minIndex = j;//assigns the smallerString as the minIndex
             }
         }
         if(i!==minIndex){
             swap(arr, i, minIndex);
         }
     }
+    return arr;
+}
+
+function compare(a, b){
+    return a > b;
+}
+
+function compareString(firstString, secondString){
+    let length = firstString.length > secondString.length ? secondString.length : firstString.length;
+    let i=0;
+    while(i<length+1){
+        let a  = firstString.charAt(i);
+        let b = secondString.charAt(i);
+
+        if(a==b){
+            i++;
+        } else {
+            let result = a > b;
+            return result;
+        }
+
+    }
+    
+    return firstString.length > secondString.length;
+
+   
 }
